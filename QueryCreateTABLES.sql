4f6225f59377708e9f5e250324399c900e3cb6e5
SejiMe <44836805+SejiMe@users.noreply.github.com>
2021-06-20 17:45:52 +0800

    Update QueryCreateTABLES.sql

--- QueryCreateTABLES.sql
+++ QueryCreateTABLES.sql
@@ -10,10 +10,12 @@ CREATE TABLE Brand
 
 CREATE TABLE ProductType
 (
-	ID INT PRIMARY KEY IDENTITY (1,1),
-	ProductTypeName VARCHAR(50)
-);
+	ID INT IDENTITY (1,1),
+	ProductTypeName VARCHAR(50),
 
+	CONSTRAINT PK_ProductType PRIMARY KEY(ID)
+
+);
 
 CREATE TABLE Product
 (
@@ -24,6 +26,7 @@ CREATE TABLE Product
 	BrandID INT,
 	ProductTypeID INT,
 
+	-- FK AND PK
 	CONSTRAINT PK_Product PRIMARY KEY(ID),
 	CONSTRAINT FK_ProductBrand FOREIGN KEY (BrandID) REFERENCES Brand(ID),
 	CONSTRAINT FK_ProductProductType FOREIGN KEY (ProductTypeID) REFERENCES ProductType(ID)
@@ -34,9 +37,8 @@ CREATE TABLE BottleSacks
 (
 	ID INT IDENTITY(1,1),
 	Quantity INT, -- Amount of bottle stored inside this sack of one ID --
-		
+	Stat VARCHAR(25),	-- to Valuate if its sold or not and not been sold
 	ProductID INT,
 	CONSTRAINT PK_BottleSacks PRIMARY KEY(ID),
 	CONSTRAINT FK_BottleProduct FOREIGN KEY (ProductID) REFERENCES	Product(ID)
-
 );
